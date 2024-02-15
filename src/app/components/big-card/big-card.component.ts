4bd786b9237615eab03212a30626b0e64f533f6b
Isadora Gomes <isadoragnascimento07@gmail.com>
2024-02-15 10:02:02 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -9,12 +9,12 @@ export class BigCardComponent implements OnInit {
 
   @Input()
   photoCover:string ="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
-
   @Input()
   cardTitle:string =""
-
   @Input()
   cardDescription:string =""
+  @Input()
+  Id:string="0"
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia novo filme do Homem de Ferro, confira: ",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "2",
+    "title": "Nova série anunciada no Disney+",
+    "description": "bla bla bla bla",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+];
